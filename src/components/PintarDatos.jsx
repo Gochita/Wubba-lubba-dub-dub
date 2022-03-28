25c257f3288e2071e53caa1244edcc4b88e11fe9
tamagochita <mevere66@gmail.com>
2022-03-27 20:47:07 -0500

    bug arreglado error no encontrado

--- src/components/PintarDatos.jsx
+++ src/components/PintarDatos.jsx
@@ -9,43 +9,39 @@ const PintarDatos = ({ nombrePersonaje }) => {
     consumirApi(nombrePersonaje);
   }, [nombrePersonaje]);
 
-const creaPeticion = axios.create();
-
-
+  const creaPeticion = axios.create();
 
   const consumirApi = async (nombre) => {
     try {
-     
-       // para vivos `https://rickandmortyapi.com/api/character/?name=${nombre}&status=alive`
-       // para muertos `https://rickandmortyapi.com/api/character/?name=${nombre}&status=dead`
-    
-      const res = await creaPeticion.get(`https://rickandmortyapi.com/api/character/?name=${nombre}`)
-      if (!res.request) {
-        return Swal.fire({
-          title: "!Error",
-          text: "💀 oops, no results found 💀",
-          icon: "error",
-          confirmButtonText: "confirm",
-        });
-      }
+      // para vivos `https://rickandmortyapi.com/api/character/?name=${nombre}&status=alive`
+      // para muertos `https://rickandmortyapi.com/api/character/?name=${nombre}&status=dead`
 
-      setPersonajes(res.data.results);
+        await creaPeticion
+        .get(`https://rickandmortyapi.com/api/character/?name=${nombre}`)
+        .then((response) => {
+          setPersonajes(response.data.results);
+        })
+        .catch(() => {
+          Swal.fire({
+            title: "!Error",
+            text: "💀 oops, no results found 💀",
+            icon: "error",
+            confirmButtonText: "confirm",
+          });
+        });
+    
     } catch (error) {
       console.log(error);
     } finally {
-      
     }
-    
   };
 
   return (
-   
-      <div className="row mt-2 mb-5" >
-        {personajes.map((item) => (
-          <Personaje key={item.id} personaje={item} />
-        ))}
-      </div>
-
+    <div className="row mt-2 mb-5">
+      {personajes.map((item) => (
+        <Personaje key={item.id} personaje={item} />
+      ))}
+    </div>
   );
 };
 
